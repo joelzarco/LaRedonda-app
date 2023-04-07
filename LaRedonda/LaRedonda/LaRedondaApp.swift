1f2609502573f57a56ab74766f99718527fa5b83
sergio joel camacho zarco <czarco@sergios-MacBook-Air.local>
2023-04-07 15:52:51 -0600

    LaRedonda App: Image assets structured and product model created

---
+++ LaRedonda/LaRedonda/LaRedondaApp.swift
@@ -0,0 +1,17 @@
+//
+//  LaRedondaApp.swift
+//  LaRedonda
+//
+//  Created by sergio joel camacho zarco on 06/04/23.
+//
+
+import SwiftUI
+
+@main
+struct LaRedondaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
