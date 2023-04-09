8e280ab9757119f643a917fb4c1bc44580b87b96
joelzarco <joelzarco@yahoo.com.mx>
2023-04-09 08:05:39 -0600

    Main userInterface created with LazyVGrid, divided into sections for each wine type

--- LaRedonda/LaRedonda/ContentView.swift
+++ LaRedonda/LaRedonda/ContentView.swift
@@ -8,19 +8,85 @@
 import SwiftUI
 
 struct ContentView: View {
+//    @StateObject var cartManager = CartManager()
+    
+    var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+        
+        NavigationView{
+            ScrollView{
+                LazyVGrid(columns: columns, spacing: 20){
+                    Section{
+                        ForEach(tintoList, id: \.id){ product in
+                            NavigationLink{
+                                WineDetailView(product: product).environmentObject(cartManager)
+                            } label: {
+                                WineCard(product: product).environmentObject(cartManager)
+                            }.foregroundColor(.primary) // to avoid accent
+                        }
+                    } header: {
+                        Text("Tinto")
+                            .font(.title2)
+                            .bold()
+                    }
+                    
+                    Section{
+                        ForEach(espumosoList, id: \.id){ product in
+                            NavigationLink{
+                                WineDetailView(product: product).environmentObject(cartManager)
+                            } label: {
+                                WineCard(product: product).environmentObject(cartManager)
+                            }.foregroundColor(.primary)
+                        }
+                    } header: {
+                        Text("Espumoso")
+                            .font(.title2)
+                            .bold()
+                    }
+                    
+                    Section{
+                        ForEach(blancoList, id: \.id){ product in
+                            NavigationLink{
+                                WineDetailView(product: product).environmentObject(cartManager)
+                            } label: {
+                                WineCard(product: product).environmentObject(cartManager)
+                            }.foregroundColor(.primary)
+                        }
+                    } header: {
+                        Text("Blanc")
+                            .font(.title2)
+                            .bold()
+                    }
+                    
+                    Section{
+                        ForEach(roseList, id: \.id){ product in
+                            NavigationLink{
+                                WineDetailView(product: product).environmentObject(cartManager)
+                            } label: {
+                                WineCard(product: product).environmentObject(cartManager)
+                            }.foregroundColor(.primary)
+                        }
+                    } header: {
+                        Text("Rosé")
+                            .font(.title2)
+                            .bold()
+                    }
+                } // lazyV
+                .padding()
+            } // scrllV
+            .navigationTitle("Viñedos LaRedonda")
+            .navigationBarTitleDisplayMode(.automatic)
+            .toolbar {
+                NavigationLink{ // dest
+                    CartView().environmentObject(cartManager)
+                } label: { // origin
+                    CartButton(numberOfProducts: cartManager.products.count)
+                }
+            } // tool
+        } // nav
+        .preferredColorScheme(.dark)
+    } // someV
 }
 
-//struct ContentView_Previews: PreviewProvider {
-//    static var previews: some View {
-//        ContentView()
-//    }
-//}
+
