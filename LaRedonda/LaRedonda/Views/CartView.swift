4168f57ea478502cba09545d58bc2cf36e0ca3c0
JohelCZarco <101434425+joelzarco@users.noreply.github.com>
2023-04-10 14:45:24 -0600

    CartView created for shopping summary display and attached in .toolBar() Buttton
    CartView created for shopping summary display and attached in .toolBar() Buttton

---
+++ LaRedonda/LaRedonda/Views/CartView.swift
@@ -0,0 +1,36 @@
+//
+//  CartView.swift
+//  LaRedonda
+//
+//  Created by sergio joel camacho zarco on 10/04/23.
+//
+
+import SwiftUI
+
+struct CartView: View {
+    @EnvironmentObject var cartManager : CartManager
+    
+    var body: some View {
+//        Text("Number of items: \(cartManager.products.count)")
+        ScrollView{
+            if(cartManager.products.count > 0){
+                ForEach(cartManager.products, id: \.id){ product in
+                    WineRow(product: product)
+                }
+                HStack{
+                    Text("Your cart total is: ")
+                    Spacer()
+                    Text("$\(cartManager.total).00")
+                        .bold()
+                }
+//
+            } else{
+                Text("Your cart is empty")
+            }
+
+        } //scrllV
+        .navigationTitle("My Cart")
+        .padding(.top)
+        
+    }
+}
