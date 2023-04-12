b30703c03a6ee359bc2f72ebbc30a17199757b9f
JohelCZarco <101434425+joelzarco@users.noreply.github.com>
2023-04-12 08:41:27 -0600

    addToCart button in WineDetailView refactored
    change in button style and layout

--- LaRedonda/LaRedonda/Views/WineDetailView.swift
+++ LaRedonda/LaRedonda/Views/WineDetailView.swift
@@ -8,12 +8,13 @@
 import SwiftUI
 
 struct WineDetailView: View {
+    
     @EnvironmentObject var cartManager : CartManager
     var product : Product
     
     var body: some View {
-        ZStack(alignment: .topTrailing) {
-            ScrollView{
+        // from Zstack to VS to just scrll
+            ScrollView {
                 TabView{
                     ForEach(0..<product.imageNames.count){ i in
                         Image("\(product.imageNames[i])")
@@ -21,87 +22,98 @@ struct WineDetailView: View {
                             .cornerRadius(10)
                             .scaledToFit()
                             .frame(width: 350)
-                    }
+                    } //fr
                 } // tabV
                 .tabViewStyle(PageTabViewStyle())
                 .indexViewStyle(PageIndexViewStyle(backgroundDisplayMode: .never))
                 .frame(height: 400)
-                HStack{
-                    VStack{
-                        Text("Cepa:")
-                            .bold()
+                Group{
+                    HStack{
+                        VStack{
+                            Text("Cepa:")
+                                .bold()
+                                .font(.body)
+                            Text(product.cepa)
+                                .font(.caption)
+                        }
+                        VStack{
+                            Text("Crianza:")
+                                .font(.body).bold()
+                            Text(product.crianza)
+                                .font(.caption)
+                        }
+                        VStack{
+                            Text("Pres:").font(.body).bold()
+                            Text(product.presentacion)
+                                .font(.caption)
+                        }
+                    }
+                    Spacer(minLength: 5)
+                    VStack(alignment: .leading){
+                        Text(product.descripcion)
                             .font(.body)
-                        Text(product.cepa)
-                            .font(.caption)
+                            .frame(width: 350)
+                            .allowsTightening(true)
+                            .truncationMode(.middle)
+                            .padding(.top)
                     }
-                    VStack{
-                        Text("Crianza:")
-                            .font(.body).bold()
-                        Text(product.crianza)
-                            .font(.caption)
-                    }
-                    VStack{
-                        Text("Pres:").font(.body).bold()
-                        Text(product.presentacion)
-                            .font(.caption)
-                    }
-                }
-                Spacer(minLength: 5)
-                VStack(alignment: .leading){
-                    Text(product.descripcion)
-                        .font(.body)
-                        .frame(width: 350)
+                    Spacer()
+                    VStack(alignment : .leading){
+                        Text("Vista:")
+                            .bold()
+                        Text(product.vista)
+                        
+                        Text("Nariz:")
+                            .bold()
+                        Text(product.nariz)
+                        
+                        Text("Boca:")
+                            .bold()
+                        Text(product.boca)
+                    }.frame(width: 350)
                         .allowsTightening(true)
                         .truncationMode(.middle)
                         .padding(.top)
-                }
-                Spacer()
-                VStack(alignment : .leading){
-                    Text("Vista:")
-                        .bold()
-                    Text(product.vista)
-                    
-                    Text("Nariz:")
-                        .bold()
-                    Text(product.nariz)
-                    
-                    Text("Boca:")
-                        .bold()
-                    Text(product.boca)
-                }.frame(width: 350)
-                    .allowsTightening(true)
-                    .truncationMode(.middle)
+                    HStack{
+                        VStack{
+                            Text("Temp. de consumo:")
+                                .bold()
+                            Text(product.temperatura)
+                                .font(.caption)
+                        }
+                        VStack{
+                            Text("Graduacion:")
+                                .bold()
+                            Text(product.graduacion)
+                                .font(.caption)
+                        }
+                    }
                     .padding(.top)
-                
-                HStack{
-                    VStack{
-                        Text("Temp. de consumo:")
+                    VStack(alignment: .leading) {
+                        Text("Sinergia gastronomica sugerida:")
                             .bold()
-                        Text(product.temperatura)
-                            .font(.caption)
+                        Text(product.gastronomia)
+                            .allowsTightening(true)
+                            .truncationMode(.middle)
                     }
-                    
-                    VStack{
-                        Text("Graduacion:")
+                    .padding(.top)
+                    Spacer()
+                    HStack{
+                        Text("Precio: $\(product.price).00 MXN")
                             .bold()
-                        Text(product.graduacion)
-                            .font(.caption)
                     }
+                    .padding(.top)
+                } // Grp
+                //
+                Button("Add to cart"){
+                    cartManager.addToCart(product: product)
                 }
-                .padding(.top)
-                VStack(alignment: .leading) {
-                    Text("Sinergia gastronomica sugerida:")
-                        .bold()
-                    Text(product.gastronomia)
-                        .allowsTightening(true)
-                        .truncationMode(.middle)
-                }.padding(.top)
-                Spacer()
-                HStack{
-                    Text("Precio: $\(product.price).00 MXN")
-                        .bold()
-                }.padding(.top)
-            } // VS
+                .buttonStyle(.bordered).bold()
+                .cornerRadius(10)
+                .padding(10)
+                .foregroundColor(.primary)
+
+            } // Scroll
             .navigationTitle("\(product.name)")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
@@ -110,20 +122,7 @@ struct WineDetailView: View {
                 } label: {
                     CartButton(numberOfProducts: cartManager.products.count)
                 }
-            }
-            
-            Button{
-                cartManager.addToCart(product: product)
-            } label: {
-                Image(systemName: "plus")
-                    .padding(10)
-                    .foregroundColor(.primary)
-                    .background(.black)
-                    .cornerRadius(50)
-                    .padding(.top , 50)
-                    .padding(.trailing, 40)
-            }
-        } // ZS
-    }
-}
+            } // toolB
+    } // someV
+} // main
 
